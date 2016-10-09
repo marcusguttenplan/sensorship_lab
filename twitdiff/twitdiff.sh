78dee39991bb03501c7af3b9b7987686cddbc3d5
marcusguttenplan <marcusguttenplan@gmail.com>
2016-10-09 16:51:56 -0700

    use sort and uniq to diff

--- twitdiff/twitdiff.sh
+++ twitdiff/twitdiff.sh
@@ -1,11 +1,20 @@
-##!/bin/bash
+#!usr/local/bin/bash
   echo "Enter Users to Scrape & Diff: \n______________________________________ \n"
   read -a userlist
   echo "______________________________________ \n"
   for i in "${userlist[@]}"
   do
-     echo $i # or do whatever with individual element of the array
-     t followers $i >> $i.txt
-     watch -n 20 'bash -c "diff <(sort $i.txt) $i.txt"'
+    echo $i # or do whatever with individual element of the array
+    t followers $i >> $i.txt
+    sort -d $i.txt -o $i.diff.txt
+    sort -u $i.diff.txt -o $i.diff.txt
+    sort -u $i.txt -o $i.txt
+    # colordiff $i.txt $i.diff.txt
+    diff --old-group-format=$'\e[0;31m%<\e[0m' \
+     --new-group-format=$'\e[0;31m%>\e[0m' \
+     --unchanged-group-format=$'\e[0;32m%=\e[0m' \
+     $i.txt $i.diff.txt
+    # watch -n 20 'bash -c "diff <(sort -u '$i.txt' -o '$i.txt') '$i.txt'"'
+    # watch -n 20 'bash -c "cut -d '' -f1 '$i.txt' | uniq -d"'
   done
   # cd "$userlist" && atom . && gulp
