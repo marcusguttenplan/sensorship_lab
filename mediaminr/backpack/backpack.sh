4610cb140c3a25c5ac146333e24a21715838f115
marcusguttenplan <marcusguttenplan@gmail.com>
2016-10-25 14:35:15 -0700

    add help to backpack script

--- mediaminr/backpack/backpack.sh
+++ mediaminr/backpack/backpack.sh
@@ -1,7 +1,32 @@
+help() {
+	echo "\n"
+	echo "\n"
+	echo " backpack.sh - website capture and differencer"
+	echo " _____________________________________________"
+  echo " "
+  echo " sh backpack [options], i.e. sh backpack.sh crawler"
+  echo " "
+  echo " options:"
+	echo " "
+  echo " crawler	crawl archive for site"
+  echo " finder   	find crawled HTML folders"
+	echo " opener		open crawled HTML folders in browser"
+	echo " differ		find differences in crawled HTML"
+	echo "\n"
+	echo "\n"
+	exit 0
+}
+
+
+
 finder() {
-	echo "Enter Directory: \n______________________________________ \n"
+	echo " "
+	echo "Enter directory:"
+	echo "_____________________________________________"
+	echo " "
   read -a dir
-  echo "______________________________________ \n"
+  echo "_____________________________________________"
+	echo " "
   for i in "${dir[@]}"
   do
     echo $i # or do whatever with individual element of the array
@@ -13,23 +38,32 @@ finder() {
 }
 
 opener() {
-	echo "Enter Directory: \n______________________________________ \n"
+	echo " "
+	echo "Enter directory:"
+	echo "_____________________________________________"
+	echo " "
   read -a dir
-  echo "______________________________________ \n"
+  echo "_____________________________________________"
+	echo " "
   for i in "${dir[@]}"
   do
     echo $i # or do whatever with individual element of the array
 		cd $i
 		find . -iname '*.html' | while read line; do
 			echo "Finding HTML: $line";
+			open $line
 		done
   done
 }
 
 differ() {
-	echo "Enter Directory: \n______________________________________ \n"
+	echo " "
+	echo "Enter directory:"
+	echo "_____________________________________________"
+	echo " "
   read -a dir
-  echo "______________________________________ \n"
+  echo "_____________________________________________"
+	echo " "
   for i in "${dir[@]}"
   do
     echo $i # or do whatever with individual element of the array
@@ -38,11 +72,15 @@ differ() {
   done
 }
 
-backpack() {
-	echo "Enter Site to Scrape & Diff: \n______________________________________ \n"
+crawler() {
+	echo " "
+	echo "Enter websites:"
+	echo "_____________________________________________"
+	echo " "
   read -a sitelist
-  echo "______________________________________ \n"
-  for i in "${sitelist[@]}"
+	echo "_____________________________________________"
+	echo " "
+	for i in "${sitelist[@]}"
   do
     echo $i # or do whatever with individual element of the array
 		waybackpack $i -d $i
