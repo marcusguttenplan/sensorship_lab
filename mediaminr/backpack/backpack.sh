23cb1897bee59572149353779286b775fbb2d1d1
marcusguttenplan <marcusguttenplan@gmail.com>
2016-10-25 13:57:22 -0700

    add options to backpack script

--- mediaminr/backpack/backpack.sh
+++ mediaminr/backpack/backpack.sh
@@ -1,21 +1,52 @@
-
-
 finder() {
-	find . -iname '*.html' | while read line; do
-		echo "Finding HTML: $line";
-#		open "$line"
-	done
+	echo "Enter Directory: \n______________________________________ \n"
+  read -a dir
+  echo "______________________________________ \n"
+  for i in "${dir[@]}"
+  do
+    echo $i # or do whatever with individual element of the array
+		cd $i
+		find . -iname '*.html' | while read line; do
+			echo "Finding HTML: $line";
+		done
+  done
 }
 
 opener() {
-	find . -iname '*.html' | while read line; do
-		# echo "Finding HTML and opening $line";
-		open "$line"
-	done
+	echo "Enter Directory: \n______________________________________ \n"
+  read -a dir
+  echo "______________________________________ \n"
+  for i in "${dir[@]}"
+  do
+    echo $i # or do whatever with individual element of the array
+		cd $i
+		find . -iname '*.html' | while read line; do
+			echo "Finding HTML: $line";
+		done
+  done
 }
 
 differ() {
-	find . -iname "*.html" | xargs diff -ry --from-file * | colordiff;
+	echo "Enter Directory: \n______________________________________ \n"
+  read -a dir
+  echo "______________________________________ \n"
+  for i in "${dir[@]}"
+  do
+    echo $i # or do whatever with individual element of the array
+		cd $i
+		find . -iname "*.html" | xargs diff -ry --from-file * | colordiff;
+  done
+}
+
+backpack() {
+	echo "Enter Site to Scrape & Diff: \n______________________________________ \n"
+  read -a sitelist
+  echo "______________________________________ \n"
+  for i in "${sitelist[@]}"
+  do
+    echo $i # or do whatever with individual element of the array
+		waybackpack $i -d $i
+  done
 }
 
 "$@"
